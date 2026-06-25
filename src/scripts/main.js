ef1292955e8588386712dddc18eb3653375e7eed
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-06-25 19:24:01 -0300

    preparando para o merge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const form = document.querySelector('.message__form');
+const sliderButtons = document.querySelectorAll('.slider__button');
+
+if (form) {
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (!form.checkValidity()) {
+      form.reportValidity();
+
+      return;
+    }
+
+    form.reset();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+}
+
+sliderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.blur();
+  });
+});
